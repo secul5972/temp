fee77d54ce64f52086a9dc36542ef9163483ad12
secul5972 <secul5972@naver.com>
2021-07-04 14:15:53 +0900

    clear

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/seungcoh/doing/temp
-# Build directory: /Users/seungcoh/doing/temp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
