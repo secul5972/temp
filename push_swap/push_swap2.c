f86a1f6bda7a87acf0302d3e1edae39d0d5cb542
secul5972 <secul5972@naver.com>
2021-07-09 00:59:27 +0900

    div

--- push_swap/push_swap2.c
+++ push_swap/push_swap2.c
@@ -6,15 +6,15 @@
 /*   By: seungcoh <seungcoh@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/09 00:27:03 by seungcoh          #+#    #+#             */
-/*   Updated: 2021/07/09 00:48:58 by seungcoh         ###   ########.fr       */
+/*   Updated: 2021/07/09 00:58:50 by seungcoh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	small_div(t_list *a, t_list *b, int f_flag)
+void	small_div(t_list *a, t_list *b, int h_flag)
 {
-	if (f_flag)
+	if (h_flag)
 		p_ins(a, b, 2);
 	else
 	{
@@ -22,9 +22,9 @@ void	small_div(t_list *a, t_list *b, int f_flag)
 	}
 }
 
-void	middle_div(t_list *a, t_list *b, int f_flag)
+void	middle_div(t_list *a, t_list *b, int h_flag, int *cnt)
 {
-	if (f_flag)
+	if (h_flag)
 	{
 		p_ins(a, b, 2);
 		r_ins(a, b, 2, 0);
@@ -33,12 +33,12 @@ void	middle_div(t_list *a, t_list *b, int f_flag)
 	{
 
 	}
-	
+	(*cnt)++;
 }
 
-void	big_div(t_list *a, t_list *b, int f_flag)
+void	big_div(t_list *a, t_list *b, int h_flag)
 {
-	if (f_flag)
+	if (h_flag)
 		r_ins(a, b, 1, 0);
 	else
 	{
